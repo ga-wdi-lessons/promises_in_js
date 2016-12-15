e25c49eddf06db875b5d21394da3ccf8c8688433
James Reichard <jpqreichard@gmail.com>
2016-12-15 17:03:42 -0500

    add then method

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-//someValue doesn't need to be defined within someFunction, it could be passed
-//in as an argument to someFunction
-function someFunction(someValue, callback) {
-  callback(someValue);
+//we could also return an object containing a function-expression defining a method named 'then'
+function someFunction(value) {
+  return {
+    then: callback => {
+      return callback(value);
+    }
+  };
 }
 
-someFunction( 14, value => {
+//and invoke it in the following way:
+someFunction(14).then(value => {
   console.log(`${value} was passed in as an arg to callback`);
 })
